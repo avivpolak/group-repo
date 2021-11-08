262ace95e87ac913f31a714fafbc3b7131d71b94
[aviv polak] <[avivp0437@gmail.com]>
2021-11-08 17:12:36 +0200

    hgj

--- 3b/notes_front.js
+++ 3b/notes_front.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 const baseUrl = "http://localhost:3001/api/notes";
-
+document.getElementById("root").append(getAll);
 const getAll = () => {
     const request = axios.get(baseUrl);
     return request.then((response) => response.data);
 };
 
 // ...
-
-export default { getAll, create, update };
