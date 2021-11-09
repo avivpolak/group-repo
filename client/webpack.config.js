50204e030f7e3ad5945341b181f3435ba1f4e60f
[aviv polak] <[avivp0437@gmail.com]>
2021-11-09 07:43:07 +0200

    its clear now , ready for deployment

---
+++ client/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require(`path`);
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
+module.exports = {
+    mode: `production`, //development
+    experiments: {
+        topLevelAwait: true,
+    },
+    entry: {
+        main: path.resolve(__dirname, `./src/src.js`),
+    },
+    output: {
+        path: path.resolve(__dirname, `dist`),
+        filename: `[name].js`,
+        clean: true,
+    },
+
+    //plugins
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: `group-project`,
+            filename: `phonebook.html`,
+            template: path.resolve(__dirname, `./src/phonebook.html`),
+        }),
+    ],
+};
